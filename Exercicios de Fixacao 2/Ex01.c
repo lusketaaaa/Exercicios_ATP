2e5ff2f7690b47cd93db7d8cfbea2b76ff06441a
lusketa <155599740+lusketaaaa@users.noreply.github.com>
2025-02-22 13:27:31 -0300

    Update Ex01.c

--- Exercicios de Fixacao 2/Ex01.c
+++ Exercicios de Fixacao 2/Ex01.c
@@ -1,1 +1,18 @@
+// Faca um algoritmo que receba quatro notas de um aluno, calcule e exiba a media aritmetica das 
+// notas e a mensagem de "Aprovado" para media superior ou igual a 7.0 ou a mensagem de reprovado
+// para media inferior a 7.0
 
+#include <stdio.h>
+int main() {
+    float nota1, nota2, nota3, nota4, media;
+    printf("Digite as notas do aluno: \n");
+    scanf("%f %f %f %g", &nota1, &nota2, &nota3, &nota4);
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+    printf("Media:%2.f\n", media);
+    if(media >= 7.0) {
+        printf("Aprovado\n");
+    } else {
+        printf("Reprovado\n");
+    }
+    return 0;
+}
